1689b4fc76251b6cf3af9bb9335d333b684effc7
Linusnie <linusnie@kth.se>
2016-08-01 17:59:15 +0200

    add utility functions and update ControlSysId.jl

--- src/ControlSysId.jl
+++ src/ControlSysId.jl
@@ -6,29 +6,40 @@ export
         LTIModel,
         ArxModel,
         ArmaxModel,
+        OeModel,
+        BjModel,
         StateSpaceModel,
         iddata,
         # utilities
         detrend,
         detrend!,
         dare,
+        compare,
+        lsim,
         # identification methods
         arx,
         armax,
-        n4sid
+        oe,
+        bj,
+        n4sid,
+        pred
 
 
 using Requires, Polynomials, Optim
 
 import Base.==
+import Optim.DualNumbers: Dual, realpart
 
 include("types/iddata.jl")
 include("types/idModels.jl")
 
 include("utilities.jl")
 include("statespace.jl")
+include("PEM.jl")
 include("arx.jl")
 include("armax.jl")
+include("oe.jl")
+include("bj.jl")
 include("tzero.jl")
 
 end
